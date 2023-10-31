5db6bb4eec0e1abaec35b884ce88c13453d2ec01
XxWhitepegasusxX <alexanderalmeida3040@gmail.com>
2023-10-31 18:29:17 -0300

    bottomsheetmodal added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from 'expo-router';
 import CustomHeader from '../components/CustomHeader';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 
 export const unstable_settings = {
@@ -10,10 +11,12 @@ export const unstable_settings = {
 
 export default function RootLayoutNav() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{
-        header: () => <CustomHeader/>
-      }} />
-    </Stack>
+    <BottomSheetModalProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{
+          header: () => <CustomHeader/>
+        }} />
+      </Stack>
+    </BottomSheetModalProvider>
   );
 }
--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,8 +1,10 @@
 import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity, Image, TextInput } from 'react-native'
-import React from 'react'
+import React, { useRef } from 'react'
 import { Ionicons } from '@expo/vector-icons'
 import Colors from '@/constants/Colors'
 import { Link } from 'expo-router'
+import BottomSheet from './BottomSheetModal'
+import { BottomSheetModal } from '@gorhom/bottom-sheet'
 
 const SearchBar = () => (
     <View style={styles.searchContainer}>
@@ -21,13 +23,20 @@ const SearchBar = () => (
 )
 
 export default function CustomHeader() {
+    const bottomSheetRef = useRef<BottomSheetModal>(null);
+
+    const openModal = () => {
+        bottomSheetRef.current?.present();
+    };
+    
   return (
     <SafeAreaView style={styles.safeArea}>
+        <BottomSheet ref={bottomSheetRef} />
         <View style={styles.container}>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={openModal}>
                 <Image style={styles.bike} source={require('@/assets/images/bike.png')}/>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.titleContainer}>
+            <TouchableOpacity style={styles.titleContainer} onPress={openModal}>
                 <Text style={styles.title}>Delivery • Now</Text>
                 <View style={styles.locationName}>
                     <Text style={styles.subtitle}>London</Text>
